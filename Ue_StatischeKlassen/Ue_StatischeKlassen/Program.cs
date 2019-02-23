c5dd08fa5efc9826337aedac8a851309976b9d6a
j-p-l-f <janvogt@gmx.ch>
2019-02-23 12:15:18 +0100

    Added Project to see static class

---
+++ Ue_StatischeKlassen/Ue_StatischeKlassen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ue_StatischeKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double flaeche = Mathematik.BerechneFlaeche(10, 20);
+            Console.WriteLine(flaeche);
+            Console.ReadKey();
+      
+        }
+    }
+
+    static class Mathematik
+    {
+        //Methoden
+        public static double BerechneFlaeche(double _laenge, double _breite)
+        {
+            return _laenge * _breite;
+        }
+    }
+}
